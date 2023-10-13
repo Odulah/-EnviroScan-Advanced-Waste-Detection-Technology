7573166446118f3c0853f0042bda59dd95be4c4e
Odulah <aadim0311@gmail.com>
2023-10-14 00:33:48 +0530

    uploading with the final changes

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Aadil Maqbool',
+    author_email= 'aadim0311@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
